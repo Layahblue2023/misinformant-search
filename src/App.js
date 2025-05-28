6170d971926a075b33be225cf7d4943dc251e365
Layahblue2023 <ahonablue@georgestreetinc.com>
2025-05-28 11:18:06 -0400

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import misinformantLogo from "./assets/Misinformant.svg";
+import addBtn from "./assets/add-30.png";
+import msgIcon from "./assets/message.svg";
+import home from "./assets/home.svg";
+import saved from "./assets/bookmark.svg";
+import rocket from "./assets/rocket.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={misinformantLogo} alt="Logo" className="logo" />
+            <button className="midBtn">
+              <img src={addBtn} alt="" className="addBtn" />
+            </button>
+            <div className="upperSideBottom">
+              <button className="query">
+                <img src={msgIcon} alt="" />
+                Does bleach kill COVID-19?
+              </button>
+              <button className="query">
+                <img src={msgIcon} alt="" />
+                Who owns Greenland?
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="lowerSide">
+          <div className="listItems">
+            <img src={home} alt="" className="listitemImg" /> Home
+          </div>
+          <div className="listItems">
+            <img src={saved} alt="" className="listitemImg" /> Saved
+          </div>
+          <div className="listItems">
+            <img src={rocket} alt="" className="listitemImg" /> Upgrade to pro
+          </div>
+        </div>
+      </div>
+      <div className="main">{/* Main content goes here */}</div>
     </div>
   );
 }
